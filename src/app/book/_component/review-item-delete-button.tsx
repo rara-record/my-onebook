3c93e5340ca8cb93dc9c66b6219e72f5eaf4f2bf
BORA <a52447879@gmail.com>
2024-09-19 11:42:05 +0900

    feat: 리뷰 삭제 클라이언트 컴포넌트

---
+++ src/app/book/_component/review-item-delete-button.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { DeleteReviewAction } from '@/actions/delete-review.action';
+import { Spinner } from '@/components/ui/spinner';
+import { useActionState, useEffect, useRef } from 'react';
+
+export default function ReviewItemDeleteButton({
+  reviewId,
+  bookId,
+}: {
+  reviewId: number;
+  bookId: number;
+}) {
+  // 버튼 타입이 submit이 아니거나, 기타 다른 태그일때
+  // 리액트에서는 submit() 메서드 대신 requestSubmit() 메서드를 권장합니다.
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [state, formAction, isPending] = useActionState(
+    DeleteReviewAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  }, [state]);
+
+  return (
+    <form ref={formRef} action={formAction}>
+      <input name="reviewId" value={reviewId} hidden />
+      <input name="bookId" value={bookId} hidden />
+      <div
+        onClick={() => formRef.current?.requestSubmit()}
+        className="cursor-pointer"
+      >
+        {isPending ? <Spinner /> : <span>삭제하기</span>}
+      </div>
+    </form>
+  );
+}
