e0dbc7aa63e8ce2f77082290268dada6b8c06e66
BORA <a52447879@gmail.com>
2024-09-22 17:27:32 +0900

    fix: Index Page의 static 페이지로 동작합니다.

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,7 +1,6 @@
 import { BookItem } from '@/components/book-item';
 import { BookData } from '@/types/book';
-import { delay } from '@/utils/delay';
-import { Suspense } from 'react';
+import { getRemoteImageBase64 } from '@/utils/getBase64';
 import type { Metadata } from 'next';
 import { BookListSkeleton } from './_component/book-list-skeleton';
 
@@ -20,22 +19,21 @@ export default function Home() {
     <div className="flex flex-col gap-5">
       <section>
         <h1 className="mb-0 text-lg font-bold">지금 추천하는 도서</h1>
-        <Suspense fallback={<BookListSkeleton count={3} />}>
+        {/* <Suspense fallback={<BookListSkeleton count={3} />}> */}
           <RandomBooks />
         {/* </Suspense> */}
       </section>
       <section>
         <h1 className="mb-0 text-lg font-bold">등록된 모든 도서</h1>
-        <Suspense fallback={<BookListSkeleton count={10} />}>
+        {/* <Suspense fallback={<BookListSkeleton count={10} />}> */}
           <AllBooks />
-        </Suspense>
+        {/* </Suspense> */}
       </section>
     </div>
   );
 }
 
 const RandomBooks = async () => {
-  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -56,7 +54,6 @@ const RandomBooks = async () => {
 };
 
 const AllBooks = async () => {
-  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/book`,
     { cache: 'force-cache' }
@@ -75,7 +72,3 @@ const AllBooks = async () => {
     </div>
   );
 };
-
-export const dynamic = 'force-dynamic';
-// 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
-// 컴포넌트 스트리밍 예제를 위해 사용됨
