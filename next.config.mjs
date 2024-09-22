0b32de73a7ba909c38cb1c5ab9ab37f505a30dec
BORA <a52447879@gmail.com>
2024-09-22 17:21:29 +0900

    fix: comment

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,8 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  // "images.domains" 설정이 더 이상 권장되지 않습니다.
+  // 대신 "images.remotePatterns" 설정을 사용
   images: {
     remotePatterns: [
       {
